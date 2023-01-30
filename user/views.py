6bdffbb54799791d0ab1d563e7246ff0ca092ed1
Anwar AlKandari <anwaralk@Anwars-MacBook-Air.local>
2023-01-30 14:38:31 +0300

    added: home page with the events list, and user registration

--- user/views.py
+++ user/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import login
 from .forms import Register
 
 def register_view(request):
-
     form = Register()
+
+    if request.method == 'POST':
+        form = Register(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+            login(request, user)
+
+            return redirect('home')
     
     context = {
         'form': form
